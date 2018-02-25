e73175da194702f4b31aebac29dfd29afb08c737
fankai <fankai@fklj.net>
2018-02-25 19:18:09 +0800

    add basic habits page

---
+++ pages/habit.js
@@ -0,0 +1,104 @@
+//daily.js
+const log = require('../utils/log.js')
+
+const template = {
+  id: undefined,
+  name: '',
+  min: 0,
+  max: 10,
+  step: 1,
+  unit: '',
+  values: [],
+  type: 'number',
+  order: 100000,
+  once: false
+}
+
+const types = ['check', 'number', 'custom']
+const displayTypes = ['打卡', '数值', '自定义']
+
+Page({
+  data: {
+    habit: {},
+    hvalues: '',
+    displayTypes,
+    typeIndex: 0
+  },
+  getTypeIndex(type) {
+    return types.findIndex(t => t === type)
+  },
+  onLoad: function (options) {
+    log.info('options', options)
+    let hid = options.id || -1
+    let habits = wx.getStorageSync('habits') || {}
+    let habit = habits[hid] || JSON.parse(JSON.stringify(template))
+    let hvalues = habit.values.join(' ')
+    let typeIndex = this.getTypeIndex(habit.type)
+    this.setData({habit, hvalues, typeIndex})
+  },
+  changeType: function (event) {
+    log.info('change type', event.detail.value)
+    let index = event.detail.value
+    this.data.habit.type = types[index]
+    this.setData({habit: this.data.habit, typeIndex: index})
+  },
+  changeName: function (event) {
+    this.data.habit.name = event.detail.value
+    this.setData({habit: this.data.habit})
+  },
+  changeUnit: function (event) {
+    this.data.habit.unit = event.detail.value
+    this.setData({habit: this.data.habit})
+  },
+  changeValues: function (event) {
+    log.info('change hvalues', event.detail.value)
+    this.setData({hvalues: event.detail.value})
+  },
+  nav: function (event) {
+    log.info('redirect', event.target.dataset.dest)
+    wx.redirectTo({
+      url: event.target.dataset.dest
+    })
+  },
+  normalizeHabit: function(h, habits, hvalues) {
+    let valuesStr = hvalues.replace(/^\s+|\s+$/g, '')
+    if (h.type === 'custom') {
+      h.values = valuesStr.split(/\s+/)
+      h.min = 0
+      h.max = h.values.length - 1
+      h.step = 1
+      h.unit = ''
+    } else if (h.type === 'check') {
+      h.values = []
+      h.min = 0
+      h.max = 1
+      h.step = 1
+      h.unit = ''
+      h.once = true
+    } else {
+      h.values = []
+    }
+    h.id = h.id || Date.now()
+    h.order = Math.min(h.order, Object.keys(habits).length - 1)
+    return h
+  },
+  save: function () {
+    let habits = wx.getStorageSync('habits') || {}
+    let h = this.data.habit
+    h = this.normalizeHabit(h, habits, this.data.hvalues)
+    log.info('save habit', h)
+    habits[h.id] = h
+    wx.setStorageSync('habits', habits)
+    wx.redirectTo({url: 'habits'})
+  },
+  remove: function () {
+    if (this.data.habit.id > 0) {
+      let habits = wx.getStorageSync('habits') || {}
+      let h = this.data.habit
+      h.deleted = true
+      habits[h.id] = h
+      wx.setStorageSync('habits', habits)
+    }
+    wx.redirectTo({url: 'habits'})
+  }
+})
--- pages/habits.js
+++ pages/habits.js
@@ -3,7 +3,19 @@ const log = require('../utils/log.js')
 
 Page({
   data: {
+    habits: []
   },
   onLoad: function () {
+    let habits = Object.values(wx.getStorageSync('habits')) || []
+    habits = habits.filter(h => !h.deleted)
+    this.setData({
+      habits
+    })
+  },
+  nav: function (event) {
+    log.info('redirect', event.target.dataset.dest)
+    wx.redirectTo({
+      url: event.target.dataset.dest
+    })
   }
 })
